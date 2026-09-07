c8c3aedf9f9dce4a41ebecfd1b5fc81c1c220a08
Ali Akbar <saliakbar790@gmail.com>
2026-09-07 15:37:40 +0500

    Lazy load embedding model

--- ai_engine/services/embedding_service.py
+++ ai_engine/services/embedding_service.py
@@ -1,12 +1,82 @@
-from sentence_transformers import SentenceTransformer
+# from sentence_transformers import SentenceTransformer
+# from pgvector.django import CosineDistance
+# from products.models import Product
+
+
+# # Embedding model load hoga
+# embedding_model = SentenceTransformer(
+#     "BAAI/bge-base-en-v1.5"
+# )
+
+
+# def prepare_product_text(product):
+#     """
+#     Product ki important information ko
+#     ek single text mein combine karta hai.
+#     """
+
+#     text = f"""
+#     Product Name: {product.name}
+#     Category: {product.category.name}
+#     Brand: {product.brand or ""}
+#     Description: {product.description or ""}
+#     Price: {product.price}
+#     """
+
+#     return text.strip()
+
+
+# def generate_embedding(text):
+#     """
+#     Text ko 768-dimensional embedding vector
+#     mein convert karta hai.
+#     """
+
+#     embedding = embedding_model.encode(
+#         text,
+#         normalize_embeddings=True
+#     )
+
+#     return embedding.tolist()
+
+# def search_similar_products(query, limit=5):
+
+#     query_embedding = generate_embedding(query)
+
+#     products = (
+#         Product.objects
+#         .filter(embedding__isnull=False)
+#         .annotate(
+#             distance=CosineDistance(
+#                 "embedding",
+#                 query_embedding
+#             )
+#         )
+#         .order_by("distance")[:limit]
+#     )
+
+#     return products
+
+
+
 from pgvector.django import CosineDistance
 from products.models import Product
 
 
-# Embedding model load hoga
-embedding_model = SentenceTransformer(
-    "BAAI/bge-base-en-v1.5"
-)
+_embedding_model = None
+
+
+def get_embedding_model():
+    global _embedding_model
+
+    if _embedding_model is None:
+        from sentence_transformers import SentenceTransformer
+
+        _embedding_model = SentenceTransformer(
+            "BAAI/bge-base-en-v1.5"
+        )
+
+    return _embedding_model
 
 
 def prepare_product_text(product):
@@ -32,12 +102,15 @@ def generate_embedding(text):
     mein convert karta hai.
     """
 
+    embedding_model = get_embedding_model()
+
     embedding = embedding_model.encode(
         text,
         normalize_embeddings=True
     )
 
     return embedding.tolist()
+
 
 def search_similar_products(query, limit=5):
 
