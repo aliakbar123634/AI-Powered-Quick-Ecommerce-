8cc4b4c9fb6e8bfa441f85618d1802b4b6d5498a
Ali Akbar <saliakbar790@gmail.com>
2026-09-12 10:54:31 +0500

    Fix FastAPI CORS for Vercel

--- quickai_ai/app/main.py
+++ quickai_ai/app/main.py
@@ -19,6 +19,7 @@ app.add_middleware(
         "http://127.0.0.1:5174",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://ai-powered-quick-ecommerce-ql4d.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
